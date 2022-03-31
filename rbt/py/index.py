96881c4dd3956fa59c1a87fbbaa3b071dbc79aa4
Marco <marco.pluchino@gmail.com>
2022-03-31 14:30:54 +0200

    minor release

--- rbt/py/index.py
+++ rbt/py/index.py
@@ -6,13 +6,16 @@ class Index():
         sma = (lambda stock,sma:stock.Close.rolling(window).mean())
         name = f"Sma{window}"
         stock[name] = sma(stock,window) 
+        print(f"Added {name}")
         return stock
     @classmethod
     def add_rsi(self,stock,length):
     # Add Rsi column
     # https://www.roelpeters.be/many-ways-to-calculate-the-rsi-in-python-pandas/
         import pandas_ta as pta
-        stock['Rsi'] = pta.rsi(stock['Close'], length = length) # Add Rsi column
+        name = 'Rsi'
+        stock[name] = pta.rsi(stock['Close'], length = length) # Add Rsi column
+        print(f"Added {name}")
         return stock
 # Test
 def test1():
@@ -22,5 +25,8 @@ def test1():
     stock['Close'] = pd.Series(range(10,20))
     stock = Index().add_sma(stock,7)
     print(stock)
+def test2():
+    import pandas as pd
+    pd.rolling_mean(k, 3)
 if __name__ == '__main__':
     test1()
